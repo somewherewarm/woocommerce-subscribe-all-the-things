b72397d018f7086f47c48eabeb20adbd4111a8f3
Sebastien Dumont <mailme@sebastiendumont.com>
2016-04-27 22:57:56 +0200

    Correcting the last commit

--- includes/class-wcsatt-schemes.php
+++ includes/class-wcsatt-schemes.php
@@ -182,7 +182,18 @@ class WCS_ATT_Schemes {
 
 			if ( in_array( $scope, array( 'all', 'cart-item' ) ) ) {
 
+<<<<<<< HEAD
 				$product_id      = $cart_item[ 'product_id' ];
+=======
+				// This checks if the item in the cart is a variable product or any other product type.
+				if ( ! empty( $cart_item[ 'variation_id' ] ) ) {
+					//$product_id = $cart_item[ 'variation_id' ]; // Parent ID of the Variation in the cart
+					$product_id = $cart_item[ 'data' ]->id; // ID of the variation in the cart
+				} else {
+					$product_id = $cart_item[ 'product_id' ];
+				}
+
+>>>>>>> 866fcf4... Passing the correct product id to get the product subscription schemes depending on the product type
 				$product_schemes = get_post_meta( $product_id, '_wcsatt_schemes', true );
 
 				if ( $product_schemes ) {
