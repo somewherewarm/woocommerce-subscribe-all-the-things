c1806c494be855ba050886fb3a0b139c69b404ed
Sebastien Dumont <mailme@sebastiendumont.com>
2016-04-28 19:10:59 +0200

    Each variable subscription schemes are now showing and change depending on the variation the customer selects. Not finished yet.

--- assets/js/wcsatt-add-to-cart-variation.js
+++ assets/js/wcsatt-add-to-cart-variation.js
@@ -1,42 +1,107 @@
 ;(function ( $, window, document, undefined ) {
 
-	var form                  = $(this),
-			single_variation      = form.find( '.single_variation' ),
-			product               = form.closest( '.product' ),
-			product_id            = parseInt( form.data( 'product_id' ), 10 ),
-			product_variations    = form.data( 'product_variations' ),
-			reset_variations      = form.find( '.reset_variations' ),
-			template              = wp.template( 'variation-template' ),
-			unavailable_template  = wp.template( 'unavailable-variation-template' ),
-			single_variation_wrap = form.find( '.single_variation_wrap' );
+	var v                     = $(this),
+			single_variation      = v.find( '.single_variation' ),
+			product               = v.closest( '.product' ),
+			product_id            = parseInt( v.data( 'product_id' ), 10 ),
+			single_variation_wrap = v.find( '.single_variation_wrap' ),
+			form                  = v.find( '.variations_form' ),
+			force_subscription    = $( 'input[name=force_subscription]' ).val(),
+			default_status        = $( 'input[name=default_status]' ).val(),
+			prompt                = $( 'input[name=prompt]' ).val();
 
 	// When the variation is revealed
-	form.on( 'show_variation', function( event, variation ) {
+	v.on( 'show_variation', function( event, variation ) {
 		event.preventDefault();
 
-		$.each ( variation.subscription_schemes, function( key, value ) {
-			var scheme_id          = value.id;
-			var position           = value.position;
-			var discount           = value.subscription_discount;
-			var period             = value.subscription_period;
-			var period_interval    = value.subscription_period_interval;
-			var sub_price          = value.subscription_price;
-			var sub_pricing_method = value.subscription_pricing_method;
-			var sub_regular_price  = value.subscription_regular_price;
-			var sub_sale_price     = value.subscription_sale_price;
+		// Only take action if the variation is subscribable.
+		if ( variation.is_subscribable ) {
 
-			console.log('ID: ' + scheme_id);
-			console.log('Position: ' + position);
-			console.log('Discount: ' + discount);
-			console.log('Period Type: ' + period);
-			console.log('Period Intevral: ' + period_interval);
-			console.log('Subscription Price: ' + sub_price);
-			console.log('Subscription Pricing Method: ' + sub_pricing_method);
-			console.log('Subscription Regular Price: ' + sub_regular_price);
-			console.log('Subscription Sale Price: ' + sub_sale_price);
+			// Checks that this selected variation has subscription schemes.
+			if ( variation.subscription_schemes.length > 0 ) {
 
-		} );
+				var sub_html = '<h3>' + prompt + '</h3>'
+				+ '<ul class="wcsatt-convert-cart">';
+
+				if ( force_subscription == 'yes' ) {
+					var allow_one_time_option = false;
+				}
+
+				if ( allow_one_time_option && default_status != 'subscription' ) {
+					var default_subscription_scheme_id = '0';
+				} else {
+					var default_subscription_scheme_id = '1';
+				}
+
+				if ( !allow_one_time_option ) {
+
+					sub_html = sub_html + '<li>'
+						+ '<label>'
+						+ '<input type="radio" name="convert_to_sub_' + $(product_id) + '" value="0" /> '
+						+	wcsatt_add_to_cart_variation_params.none
+						+ '</label>'
+					+ '</li>';
+
+				}
+
+				$.each ( variation.subscription_schemes, function( key, value ) {
+					var scheme_id          = value.id,
+							position           = value.position,
+							discount           = value.subscription_discount,
+							period             = value.subscription_period,
+							period_interval    = value.subscription_period_interval,
+							sub_price          = value.subscription_price,
+							sub_pricing_method = value.subscription_pricing_method,
+							sub_regular_price  = value.subscription_regular_price,
+							sub_sale_price     = value.subscription_sale_price;
+
+					var price_string = {
+						'discount'           : discount,
+						'period'             : period,
+						'period_interval'    : period_interval,
+						'sub_price'          : sub_price,
+						'sub_pricing_method' : sub_pricing_method,
+						'sub_regular_price'  : sub_regular_price,
+						'sub_sale_price'     : sub_sale_price
+					};
+
+					// Get the price for this option
+					var get_price = get_price_string( price_string );
+
+					// Create new option
+					sub_html = sub_html + '<li>'
+						+ '<label>'
+						+ '<input type="radio" name="convert_to_sub_' + product_id + '" value="' + scheme_id + '" />'
+						+	'<del><span class="amount">Regular Price</span></del>'
+						+ '<ins><span class="amount">' + sub_price + '</span></ins>'
+						+ '<span class="subscription-details"> / ' + get_price + '</span>'
+						+ '</label>'
+					+ '</li>';
+
+				} ); // END each subscription schemes
+
+				sub_html = sub_html + '</ul>'; // Close the list of options
+
+				// Place all price options for this variation.
+				$('.woocommerce-variation-price').html(sub_html);
+
+			} // END if variation has subscrition schemes
+
+		} // END if variation is subscriable
 
 	} )
+
+	/**
+	 * Returns a string representing the details of the subscription option.
+	 *
+	 * For example "$20 per Month for 3 Months".
+	 */
+	function get_price_string( values ) {
+		console.log(values);
+
+		// Need to finish coding this by converting the get_price_string() function from WooCommerce Subscriptions.
+
+		return values.period;
+	}
 
 })( jQuery, window, document );
