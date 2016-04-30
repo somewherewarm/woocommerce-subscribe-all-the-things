ffa27df59d445a6b2f4d8fb12036bfe8944832b6
Sebastien Dumont <mailme@sebastiendumont.com>
2016-04-30 05:53:58 +0200

    Add subscription option for variation Javascript 90% complete. Still have discount calculations to complete and futher testing.

--- assets/js/wcsatt-add-to-cart-variation.js
+++ assets/js/wcsatt-add-to-cart-variation.js
@@ -1,11 +1,22 @@
 ;(function ( $, window, document, undefined ) {
 
 	var v                     = $(this),
-			single_variation      = v.find( '.single_variation' ),
-			product               = v.closest( '.product' ),
-			product_id            = parseInt( v.data( 'product_id' ), 10 ),
-			single_variation_wrap = v.find( '.single_variation_wrap' ),
-			form                  = v.find( '.variations_form' ),
+			currency_symbol       = wcsatt_add_to_cart_variation_params.currency_symbol,
+			currency_pos          = wcsatt_add_to_cart_variation_params.currency_pos,
+			none                  = wcsatt_add_to_cart_variation_params.none,
+			every                 = wcsatt_add_to_cart_variation_params.every,
+			day                   = wcsatt_add_to_cart_variation_params.day,
+			days                  = wcsatt_add_to_cart_variation_params.days,
+			week                  = wcsatt_add_to_cart_variation_params.week,
+			weeks                 = wcsatt_add_to_cart_variation_params.weeks,
+			month                 = wcsatt_add_to_cart_variation_params.month,
+			months                = wcsatt_add_to_cart_variation_params.months,
+			year                  = wcsatt_add_to_cart_variation_params.year,
+			years                 = wcsatt_add_to_cart_variation_params.years,
+			st_interval           = wcsatt_add_to_cart_variation_params.st_interval,
+			nd_intervals          = wcsatt_add_to_cart_variation_params.nd_intervals,
+			rd_intervals          = wcsatt_add_to_cart_variation_params.rd_intervals,
+			th_intervals          = wcsatt_add_to_cart_variation_params.th_intervals,
 			force_subscription    = $( 'input[name=force_subscription]' ).val(),
 			default_status        = $( 'input[name=default_status]' ).val(),
 			prompt                = $( 'input[name=prompt]' ).val();
@@ -14,14 +25,20 @@
 	v.on( 'show_variation', function( event, variation ) {
 		event.preventDefault();
 
+		var product_id = variation.variation_id,
+				base_price = variation.display_price,
+				checked = false;
+
 		// Only take action if the variation is subscribable.
 		if ( variation.is_subscribable ) {
 
 			// Checks that this selected variation has subscription schemes.
 			if ( variation.subscription_schemes.length > 0 ) {
 
+				console.log(variation.subscription_schemes);
+
 				var sub_html = '<h3>' + prompt + '</h3>'
-				+ '<ul class="wcsatt-convert-cart">';
+				+ '<ul class="wcsatt-convert-product">';
 
 				if ( force_subscription == 'yes' ) {
 					var allow_one_time_option = false;
@@ -33,11 +50,17 @@
 					var default_subscription_scheme_id = '1';
 				}
 
-				if ( !allow_one_time_option ) {
+				if ( allow_one_time_option ) {
+					if ( force_subscription != 'yes' ) {
+						checked = true;
+						if ( checked ) {
+							var radio_selected = 'checked="checked" ';
+						}
+					}
 
 					sub_html = sub_html + '<li>'
 						+ '<label>'
-						+ '<input type="radio" name="convert_to_sub_' + $(product_id) + '" value="0" /> '
+						+ '<input type="radio" name="convert_to_sub_' + product_id + '" value="0" ' + radio_selected + ' /> '
 						+	wcsatt_add_to_cart_variation_params.none
 						+ '</label>'
 					+ '</li>';
@@ -50,33 +73,48 @@
 							discount           = value.subscription_discount,
 							period             = value.subscription_period,
 							period_interval    = value.subscription_period_interval,
+							sub_length         = value.subscription_length,
 							sub_price          = value.subscription_price,
 							sub_pricing_method = value.subscription_pricing_method,
 							sub_regular_price  = value.subscription_regular_price,
 							sub_sale_price     = value.subscription_sale_price;
 
-					var price_string = {
+					var data = {
+						'base_price'         : base_price,
 						'discount'           : discount,
 						'period'             : period,
 						'period_interval'    : period_interval,
+						'sub_length'         : sub_length,
 						'sub_price'          : sub_price,
 						'sub_pricing_method' : sub_pricing_method,
 						'sub_regular_price'  : sub_regular_price,
 						'sub_sale_price'     : sub_sale_price
 					};
 
+					console.log(data);
+
 					// Get the price for this option
-					var get_price = get_price_string( price_string );
+					var get_price = get_sub_option_price(data);
+
+					// Get the price string for this option
+					var get_price_string = get_sub_option_price_string(data);
+
+					if ( force_subscription == 'yes' && key == 1 ) {
+						checked = true;
+						if ( checked ) {
+							var radio_selected = 'checked="checked" ';
+						}
+					}
 
 					// Create new option
 					sub_html = sub_html + '<li>'
 						+ '<label>'
-						+ '<input type="radio" name="convert_to_sub_' + product_id + '" value="' + scheme_id + '" />'
-						+	'<del><span class="amount">Regular Price</span></del>'
-						+ '<ins><span class="amount">' + sub_price + '</span></ins>'
-						+ '<span class="subscription-details"> / ' + get_price + '</span>'
+						+ '<input type="radio" name="convert_to_sub_' + product_id + '" value="' + scheme_id + '" ' + radio_selected + ' /> '
+						+ '<del><span class="amount">' + base_price + '</span></del> ' // Original Display Price
+						+ get_price
+						+ get_price_string
 						+ '</label>'
-					+ '</li>';
+						+ '</li>';
 
 				} ); // END each subscription schemes
 
@@ -89,19 +127,136 @@
 
 		} // END if variation is subscriable
 
-	} )
+	} );
 
 	/**
-	 * Returns a string representing the details of the subscription option.
-	 *
-	 * For example "$20 per Month for 3 Months".
+	 * Returns the price for the subscription option.
 	 */
-	function get_price_string( values ) {
-		console.log(values);
+	function get_sub_option_price( values ) {
+		var base_price          = values.base_price, // The current base price of the product.
+				subscription_price  = values.sub_price, // This is empty if the pricing method is set to inherit the base price.
+				pricing_method      = values.sub_pricing_method, // Inherit or Override
+				discount            = values.discount, // Percentage of the discount based on the base price.
+				regular_price       = values.sub_regular_price, // Subscription Regular Price.
+				sale_price          = values.sub_sale_price; // Subscription Sale Price.
 
-		// Need to finish coding this by converting the get_price_string() function from WooCommerce Subscriptions.
+		var price = '';
 
-		return values.period;
-	}
+		// Identify the pricing method to determin the subscription option price.
+		switch( pricing_method ) {
+			case 'override':
+				// If the sale price is available and does not match the subscription price.
+				if ( sale_price && sale_price != subscription_price ) {
+					subscription_price = sale_price;
+					console.log('Price is on for sale');
+				} else if ( regular_price && regular_price != subscription_price ) {
+					// If the regular price is available and does not match the subscription price.
+					subscription_price = regular_price;
+					console.log('Price is regular');
+				}
+
+				break;
+
+			case 'inherit':
+			default:
+				var discount_price         = (base_price / 100) * discount; // The base price divided by 100 then times by the discount percentage.
+				console.log('Discount Price: ' + discount_price);
+				var new_subscription_price = subscription_price - discount_price; // NOTE: This is not working. :(
+				subscription_price = new_subscription_price;
+				console.log('New Subscription Price: ' + new_subscription_price);
+
+				break;
+		}
+
+		price = subscription_price; // Update the price.
+
+		console.log('Subscription Price: ' + price);
+
+		// Where does the currency symbol go? Before or After the price
+		if ( currency_pos = 'left' ) {
+			price = currency_symbol + price;
+			console.log('Price before Currency Symbol');
+		} else if ( currency_pos = 'right' ) {
+			price = price + currency_symbol;
+			console.log('Price after Currency Symbol');
+		}
+
+		return '<ins><span class="amount">' + price + '</span></ins> ';
+	};
+
+	/**
+	 * Returns a string representing the details of the subscription option terms.
+	 *
+	 * For example "Per Month for 3 Months".
+	 */
+	function get_sub_option_price_string( values ) {
+		var billing_interval    = values.period_interval,
+				billing_period      = values.period,
+				subscription_length = values.sub_length;
+
+		switch (billing_period) {
+			case 'day':
+				if ( billing_interval > 1 ) {
+					var subscription_period = days;
+				} else {
+					var subscription_period = day;
+				}
+
+				break;
+
+			case 'week':
+				if ( billing_interval > 1 ) {
+					var subscription_period = weeks;
+				} else {
+					var subscription_period = week;
+				}
+
+				break;
+
+			case 'month':
+				if ( billing_interval > 1 ) {
+					var subscription_period = months;
+				} else {
+					var subscription_period = month;
+				}
+
+				break;
+
+			case 'year':
+				if ( billing_interval > 1 ) {
+					var subscription_period = years;
+				} else {
+					var subscription_period = year;
+				}
+
+				break;
+		}
+
+		console.log( 'Billing Period: For ' + billing_interval + ' ' + subscription_period );
+
+		var subscription_string = '<span class="subscription-details">';
+
+		if ( subscription_period > 0 ) {
+			if ( subscription_length != billing_interval ) {
+				// Note: If customer is billed more than per interval: "Every 2 months"
+				if ( billing_interval > 1 ) {
+					subscription_string += every + ' ' + billing_interval + ' ' + subscription_period;
+					console.log('Example: "Every 2 months"');
+				} else {
+					// Note: If customer is billed each period: "Per month"
+					subscription_string += ' / ' + subscription_period;
+					console.log('Example: "Per month"');
+				}
+			}
+		} else if ( subscription_period = 0 ) {
+			// Note: Billing period: e.g. "every week"
+			subscription_string += every + ' ' + subscription_period;
+			console.log('Example: "Every Week"');
+		}
+
+		subscription_string += '</span>';
+
+		return subscription_string;
+	};
 
 })( jQuery, window, document );
