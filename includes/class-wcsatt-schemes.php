34af620d40ed7d35708afa53f718170aba2300ad
Manos Psychogyiopoulos <psyx@somewherewarm.net>
2017-04-05 16:59:16 +0300

    Rejoice emptiness in WCS_ATT_Schemes
    Functions previously in WCS_ATT_Schemes were either "cart API" functions or "product API" functions. The class is redundant after moving these to the right places and/or introducing the product API class.

--- includes/class-wcsatt-schemes.php
+++ includes/class-wcsatt-schemes.php
@@ -20,51 +20,14 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class WCS_ATT_Schemes {
 
-	/**
-	 * Returns the active cart-level subscription scheme id, or '0' if none is set.
-	 *
-	 * @return string
-	 */
-	public static function get_active_cart_subscription_scheme_id() {
-
-		return WC()->session->get( 'wcsatt-active-scheme-id', '0' );
-	}
+	/*
+	|--------------------------------------------------------------------------
+	| Helpers (legacy)
+	|--------------------------------------------------------------------------
+	*/
 
 	/**
-	 * Returns the active subscription scheme of a cart item, or false if the cart item is a one-off purchase.
-	 *
-	 * @return string
-	 */
-	public static function get_active_subscription_scheme_id( $cart_item ) {
-
-		$active_scheme_id = isset( $cart_item[ 'wccsub_data' ][ 'active_subscription_scheme_id' ] ) ? $cart_item[ 'wccsub_data' ][ 'active_subscription_scheme_id' ] : '0';
-
-		return $active_scheme_id;
-	}
-
-	/**
-	 * Returns the active subscription scheme of a cart item, or false if the cart item is a one-off purchase.
-	 *
-	 * @return array
-	 */
-	public static function get_active_subscription_scheme( $cart_item ) {
-
-		$schemes          = self::get_subscription_schemes( $cart_item );
-		$active_scheme_id = self::get_active_subscription_scheme_id( $cart_item );
-		$active_scheme    = false;
-
-		foreach ( $schemes as $scheme ) {
-			if ( $scheme[ 'id' ] === $active_scheme_id ) {
-				$active_scheme = $scheme;
-				break;
-			}
-		}
-
-		return $active_scheme;
-	}
-
-	/**
-	 * Returns a subscription scheme by id.
+	 * Returns a subscription scheme by id. Legacy.
 	 *
 	 * @return array
 	 */
@@ -73,7 +36,7 @@ class WCS_ATT_Schemes {
 		$found_scheme = false;
 
 		foreach ( $schemes as $scheme ) {
-			if ( $scheme[ 'id' ] === $id ) {
+			if ( $scheme->get_key() === $id ) {
 				$found_scheme = $scheme;
 				break;
 			}
@@ -82,186 +45,112 @@ class WCS_ATT_Schemes {
 		return $found_scheme;
 	}
 
+	/*
+	|--------------------------------------------------------------------------
+	| Deprecated
+	|--------------------------------------------------------------------------
+	*/
+
 	/**
-	 * Returns all available subscription schemes (product-level and cart-level).
+	 * Returns the active cart-level subscription scheme id, or false if none is set.
 	 *
-	 * @return array
+	 * @return string
 	 */
-	public static function get_subscription_schemes( $cart_item, $scope = 'all' ) {
-
-		$schemes = array();
-
-		if ( WCS_ATT_Cart::is_convertible_to_sub( $cart_item ) ) {
-
-			// Get product-level subscription schemes stored in product meta.
-
-			if ( in_array( $scope, array( 'all', 'cart-item' ) ) ) {
-
-				$product         = $cart_item[ 'data' ];
-				$product_id      = $cart_item[ 'product_id' ];
-				$product_schemes = WCS_ATT_Core_Compatibility::is_wc_version_gte_2_7() ? $product->get_meta( '_wcsatt_schemes', true ) : get_post_meta( $product_id, '_wcsatt_schemes', true );
-
-				if ( $product_schemes ) {
-					foreach ( $product_schemes as $scheme ) {
-						$scheme[ 'scope' ] = 'cart-item';
-						$schemes[]         = $scheme;
-					}
-				}
-			}
-
-			// Get cart-level subscription schemes stored in WC settings.
-			// Added only if there are no product-level schemes present.
-
-			if ( in_array( $scope, array( 'all', 'cart' ) ) ) {
-
-				$cart_level_schemes = get_option( 'wcsatt_subscribe_to_cart_schemes', array() );
-
-				if ( ! empty( $cart_level_schemes ) ) {
-					foreach ( $cart_level_schemes as $scheme ) {
-						$scheme[ 'scope' ] = 'cart';
-						$schemes[]         = $scheme;
-					}
-				}
-			}
-		}
-
-		return apply_filters( 'wcsatt_subscription_schemes', $schemes, $cart_item, $scope );
+	public static function get_cart_subscription_scheme() {
+		_deprecated_function( __METHOD__ . '()', '1.2.0', 'WCS_ATT_Cart::get_cart_subscription_scheme()' );
+		return WCS_ATT_Cart::get_cart_subscription_scheme();
 	}
 
 	/**
-	 * Returns the default subscription scheme id of a cart item, or '0' if the default option is a one-off purchase.
+	 * Returns cart-level subscription schemes, available only if there are no cart-items with product-level subscription schemes.
+	 * Subscription options defined at product-level and "legacy" subscription-type products "block" the display of cart-level subscription options.
+	 *
+	 * Must be called after all cart session data has been loaded.
+	 *
+	 * @deprecated 1.2.0
+	 *
+	 * @return array|boolean
+	 */
+	public static function get_cart_subscription_schemes() {
+		_deprecated_function( __METHOD__ . '()', '1.2.0', 'WCS_ATT_Cart::get_subscription_schemes()' );
+		return WCS_ATT_Cart::get_subscription_schemes();
+	}
+
+	/**
+	 * Returns all available subscription schemes (product-level and cart-level).
+	 *
+	 * @param  array   $cart_item
+	 * @param  string  $context
+	 * @return array
+	 */
+	public static function get_subscription_schemes( $cart_item, $context = 'any' ) {
+		_deprecated_function( __METHOD__ . '()', '1.2.0', 'WCS_ATT_Cart::get_subscription_schemes()' );
+		return WCS_ATT_Cart::get_subscription_schemes( $cart_item, $context === 'cart-item' ? 'product' : $context );
+	}
+
+	/**
+	 * Returns subscription schemes used to render cart-item level options.
+	 *
+	 * @deprecated 1.2.0
+	 *
+	 * @param  array  $cart_item
+	 * @return array
+	 */
+	public static function get_cart_item_subscription_schemes( $cart_item ) {
+		_deprecated_function( __METHOD__ . '()', '1.2.0', 'WCS_ATT_Product::get_product_subscription_schemes()' );
+		return WCS_ATT_Cart::get_subscription_schemes( $cart_item, 'product' );
+	}
+
+	/**
+	 * Returns the active cart-level subscription scheme id, or false if none is set.
+	 *
+	 * @deprecated 1.2.0
+	 *
+	 * @return string
+	 */
+	public static function get_active_cart_subscription_scheme_id() {
+		_deprecated_function( __METHOD__ . '()', '1.2.0', 'WCS_ATT_Schemes::apply_subscription_scheme()' );
+		return self::get_cart_subscription_scheme();
+	}
+
+	/**
+	 * Returns the active subscription scheme of a cart item, or false if the cart item is a one-off purchase.
+	 *
+	 * @deprecated 1.2.0
+	 *
+	 * @return string
+	 */
+	public static function get_active_subscription_scheme_id( $cart_item ) {
+		_deprecated_function( __METHOD__ . '()', '1.2.0', 'WCS_ATT_Cart::get_subscription_scheme()' );
+		return WCS_ATT_Cart::get_subscription_scheme( $cart_item );
+	}
+
+	/**
+	 * Returns the active subscription scheme of a cart item, or false if the cart item is a one-off purchase.
+	 *
+	 * @deprecated 1.2.0
+	 *
+	 * @return array
+	 */
+	public static function get_active_subscription_scheme( $cart_item ) {
+		_deprecated_function( __METHOD__ . '()', '1.2.0', 'WCS_ATT_Cart::get_subscription_scheme() and WCS_ATT_Product::get_subscription_scheme()' );
+		$active_scheme_key = WCS_ATT_Cart::get_subscription_scheme( $cart_item );
+		return WCS_ATT_Product::get_subscription_scheme( $cart_item[ 'product' ], $active_scheme_key, 'object' );
+	}
+
+	/**
+	 * Returns the default subscription scheme to set on a cart item.
+	 *
+	 * @deprecated  1.2.0
+	 * @access      private
 	 *
 	 * @param  array $cart_item
 	 * @param  array $cart_level_schemes
 	 * @return string
 	 */
 	public static function set_subscription_scheme_id( $cart_item, $cart_level_schemes ) {
-
-		if ( $cart_level_schemes ) {
-
-			// Default to last setting.
-			$default_scheme_id = WC()->session->get( 'wcsatt-active-scheme-id', false );
-
-			if ( false === $default_scheme_id ) {
-
-				// Default to subscription.
-
-				if ( apply_filters( 'wcsatt_enable_cart_subscription_by_default', false ) ) {
-
-					$default_scheme    = current( $cart_level_schemes );
-					$default_scheme_id = $default_scheme[ 'id' ];
-
-				// Default to one-time.
-
-				} else {
-					$default_scheme_id = '0';
-				}
-			}
-
-		} else {
-
-			// Default to last setting.
-			$default_scheme_id = $cart_item[ 'wccsub_data' ][ 'active_subscription_scheme_id' ];
-
-			if ( false === $default_scheme_id ) {
-
-				if ( $subscription_schemes = self::get_subscription_schemes( $cart_item, 'cart-item' ) ) {
-
-					$product            = $cart_item[ 'data' ];
-					$product_id         = $cart_item[ 'product_id' ];
-					$force_subscription = WCS_ATT_Core_Compatibility::is_wc_version_gte_2_7() ? $product->get_meta( '_wcsatt_force_subscription', true ) : get_post_meta( $product_id, '_wcsatt_force_subscription', true );
-					$default_status     = WCS_ATT_Core_Compatibility::is_wc_version_gte_2_7() ? $product->get_meta( '_wcsatt_default_status', true ) : get_post_meta( $product_id, '_wcsatt_default_status', true );
-					$default_scheme_id  = '0';
-
-					if ( $force_subscription === 'yes' || $default_status === 'subscription' ) {
-						if ( ! empty( $subscription_schemes ) ) {
-							$default_scheme    = current( $subscription_schemes );
-							$default_scheme_id = $default_scheme[ 'id' ];
-						}
-					}
-				}
-			}
-		}
-
-		return apply_filters( 'wcsatt_set_subscription_scheme_id', $default_scheme_id, $cart_item, $cart_level_schemes );
+		_deprecated_function( __METHOD__ . '()', '1.2.0', 'WCS_ATT_Cart::get_subscription_scheme_to_apply()' );
 	}
-
-	/**
-	 * Returns subscription schemes for cart-item level options.
-	 * Will return either:
-	 *
-	 *  - product-level subscription schemes, when these are defined at product-level, or
-	 *  - cart-level subscription schemes, when they exist and a grouped UI can't be displayed for all cart items ( @see get_cart_subscription_schemes ).
-	 *
-	 * @param  array $cart_item
-	 * @return array
-	 */
-	public static function get_cart_item_subscription_schemes( $cart_item ) {
-
-		$cart_item_schemes = array();
-
-		// Cart-item options are displayed only if we don't have any grouped cart-level options to show.
-		if ( false === self::get_cart_subscription_schemes() ) {
-
-			$cart_item_schemes = self::get_subscription_schemes( $cart_item, 'cart-item' );
-
-			// Cart-level options are displayed at cart-item level when we can't show them grouped together.
-			if ( empty( $cart_item_schemes ) ) {
-				$cart_item_schemes = self::get_subscription_schemes( $cart_item, 'cart' );
-			}
-		}
-
-		return $cart_item_schemes;
-	}
-
-	/**
-	 * Returns subscription schemes for cart-level options.
-	 * Cart-level options will be displayed only if no cart-item is found with its own product-level subscription scheme.
-	 * This means that subscription options defined at product-level and "legacy" subscription-type products will "block" the display of cart-level options.
-	 *
-	 * In this case, cart-level options will be displayed at cart-item level.
-	 *
-	 * Must be called after all cart session data has been loaded.
-	 *
-	 * @return array|boolean
-	 */
-	public static function get_cart_subscription_schemes() {
-
-		$cart_level_schemes      = array();
-		$cart_level_schemes_keys = array();
-		$cart_level_schemes      = get_option( 'wcsatt_subscribe_to_cart_schemes', array() );
-
-		if ( empty( $cart_level_schemes ) ) {
-			return false;
-		}
-
-		foreach ( $cart_level_schemes as $cart_level_scheme ) {
-			$cart_level_schemes_keys[] = $cart_level_scheme[ 'id' ];
-		}
-
-		foreach ( WC()->cart->cart_contents as $cart_item ) {
-
-			if ( ! WCS_ATT_Cart::is_supported_product_type( $cart_item ) ) {
-				return false;
-			}
-
-			if ( $cart_item_level_schemes = self::get_subscription_schemes( $cart_item, 'cart-item' ) ) {
-				return false;
-			}
-
-			if ( WC_Subscriptions_Product::is_subscription( $cart_item[ 'product_id' ] ) ) {
-				return false;
-			}
-		}
-
-		return $cart_level_schemes;
-	}
-
-	/*
-	|--------------------------------------------------------------------------
-	| Deprecated
-	|--------------------------------------------------------------------------
-	*/
 
 	/**
 	 * Returns all available subscription schemes for displaying single-product options (product-level).
