0a530f0e5103d127c893af7deec3d439872061d0
Manos Psychogyiopoulos <psyx@somewherewarm.net>
2017-06-14 15:25:36 +0300

    Use 'woocommerce_order_item_product' instead of 'woocommerce_get_product_from_item' (SLOW) filter under WC 3.0

--- includes/class-wcsatt-order.php
+++ includes/class-wcsatt-order.php
@@ -26,7 +26,12 @@ class WCS_ATT_Order {
 		add_filter( 'woocommerce_order_again_cart_item_data', array( __CLASS__, 'restore_cart_item_from_order_item' ), 10, 3 );
 
 		// Restore the subscription state of a product instantiated using an order item as reference.
-		add_filter( 'woocommerce_get_product_from_item', array( __CLASS__, 'restore_product_from_order_item' ), 10, 3 );
+		if ( WCS_ATT_Core_Compatibility::is_wc_version_gte_2_7() ) {
+			add_filter( 'woocommerce_order_item_product', array( __CLASS__, 'restore_product_from_order_item' ), 10, 2 );
+		} else {
+			// Using this under WC 3.0 may result in serious performance issues.
+			add_filter( 'woocommerce_get_product_from_item', array( __CLASS__, 'restore_product_from_order_item' ), 10, 2 );
+		}
 
 		// Save subscription scheme in subscription item meta so it can be re-applied later.
 		if ( WCS_ATT_Core_Compatibility::is_wc_version_gte_2_7() ) {
@@ -50,11 +55,23 @@ class WCS_ATT_Order {
 	 */
 	public static function get_subscription_scheme( $order_item ) {
 		$scheme_key = null;
-		if ( isset( $order_item[ 'wcsatt_scheme' ] ) ) {
-			$scheme_key = 0 === absint( $order_item[ 'wcsatt_scheme' ] ) ? false : strval( $order_item[ 'wcsatt_scheme' ] );
-		// Backwards compatibility with v1.
-		} elseif ( isset( $order_item[ 'wcsatt_scheme_id' ] ) ) {
-			$scheme_key = 0 === absint( $order_item[ 'wcsatt_scheme_id' ] ) ? false : strval( $order_item[ 'wcsatt_scheme_id' ] );
+		// Because who trusts WC 3.0 for back-compat?
+		if ( WCS_ATT_Core_Compatibility::is_wc_version_gte_2_7() ) {
+			if ( $order_item->meta_exists( '_wcsatt_scheme' ) ) {
+				$scheme_key = $order_item->get_meta( '_wcsatt_scheme', true );
+				$scheme_key = 0 === absint( $scheme_key ) ? false : strval( $scheme_key );
+			// Backwards compatibility with v1.
+			} elseif ( $order_item->meta_exists( '_wcsatt_scheme_id' ) ) {
+				$scheme_key = $order_item->get_meta( '_wcsatt_scheme_id', true );
+				$scheme_key = 0 === absint( $scheme_key ) ? false : strval( $scheme_key );
+			}
+		} else {
+			if ( isset( $order_item[ 'wcsatt_scheme' ] ) ) {
+				$scheme_key = 0 === absint( $order_item[ 'wcsatt_scheme' ] ) ? false : strval( $order_item[ 'wcsatt_scheme' ] );
+			// Backwards compatibility with v1.
+			} elseif ( isset( $order_item[ 'wcsatt_scheme_id' ] ) ) {
+				$scheme_key = 0 === absint( $order_item[ 'wcsatt_scheme_id' ] ) ? false : strval( $order_item[ 'wcsatt_scheme_id' ] );
+			}
 		}
 		return $scheme_key;
 	}
@@ -87,10 +104,9 @@ class WCS_ATT_Order {
 	 *
 	 * @param  WC_Product  $product
 	 * @param  array       $order_item
-	 * @param  WC_Order    $order
 	 * @return WC_Product
 	 */
-	public static function restore_product_from_order_item( $product, $order_item, $order ) {
+	public static function restore_product_from_order_item( $product, $order_item ) {
 		if ( null !== ( $scheme_key = self::get_subscription_scheme( $order_item ) ) ) {
 			WCS_ATT_Product::set_subscription_scheme( $product, $scheme_key );
 		}
