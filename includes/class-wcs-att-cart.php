somewherewarm/woocommerce-subscribe-all-the-things

commit f944136373cbb06f0c8533248f5ba159bd6432c3

Prevent cart subscriptions while renewing/(re)subscribing

includes/class-wcs-att-cart.php
@@ -132,6 +132,11 @@ class WCS_ATT_Cart {
 			if ( WCS_ATT_Product::is_subscription_product_type( $cart_item[ 'data' ] ) ) {
 				return false;
 			}
+
+			// Renewing/Resubscribing?
+			if ( isset( $cart_item[ 'subscription_renewal' ] ) || isset( $cart_item[ 'subscription_initial_payment' ] ) || isset( $cart_item[ 'subscription_resubscribe' ] ) ) {
+				return false;
+			}
 		}
 
 		return $cart_level_schemes;

commit 4bbba0536f877b21a8e5a1e1f85f3b9c0908e012

Allow re-subscribing/renewing cart-level subscriptions
Broken in f944136373cbb06f0c8533248f5ba159bd6432c3

includes/class-wcs-att-cart.php
@@ -95,9 +95,10 @@ class WCS_ATT_Cart {
 	 *
 	 * @since  2.0.0
 	 *
+	 * @param  string         $context  Function call context. Values: 'cart' or 'display'.
 	 * @return array|boolean
 	 */
-	public static function get_cart_subscription_schemes() {
+	public static function get_cart_subscription_schemes( $context = 'cart' ) {
 
 		$cart_level_schemes     = array();
 		$cart_level_scheme_meta = get_option( 'wcsatt_subscribe_to_cart_schemes', array() );
@@ -133,9 +134,11 @@ class WCS_ATT_Cart {
 				return false;
 			}
 
-			// Renewing/Resubscribing?
-			if ( isset( $cart_item[ 'subscription_renewal' ] ) || isset( $cart_item[ 'subscription_initial_payment' ] ) || isset( $cart_item[ 'subscription_resubscribe' ] ) ) {
-				return false;
+			// When getting cart subscription schemes for display, do not return anything when renewing/resubscribing.
+			if ( 'display' === $context ) {
+				if ( isset( $cart_item[ 'subscription_renewal' ] ) || isset( $cart_item[ 'subscription_initial_payment' ] ) || isset( $cart_item[ 'subscription_resubscribe' ] ) ) {
+					return false;
+				}
 			}
 		}
 
@@ -363,8 +366,11 @@ class WCS_ATT_Cart {
 
 		$cart_level_schemes = self::get_subscription_schemes( $cart_item, 'cart' );
 
-		// Currently a cart item can only have product-level or cart-level schemes, not both - @see 'WCS_ATT_Cart::get_cart_subscription_schemes'.
-		if ( ! empty( $cart_level_schemes ) ) {
+		/*
+		 * Currently a cart item can only have product-level or cart-level schemes, not both - @see 'WCS_ATT_Cart::get_cart_subscription_schemes'.
+		 * Note that if there are no cart-level schemes on display, we shouldn't apply any cart-level scheme in the background.
+		 */
+		if ( ! empty( $cart_level_schemes ) && self::get_cart_subscription_schemes( 'display' ) ) {
 
 			// Read active cart scheme from session.
 			$scheme_key_to_apply = self::get_cart_subscription_scheme();

includes/display/class-wcs-att-display-cart.php
@@ -179,7 +179,7 @@ class WCS_ATT_Display_Cart {
 	public static function show_subscribe_to_cart_prompt() {
 
 		// Show cart/order level options only if all cart items share a common cart/order level subscription scheme.
-		if ( $subscription_schemes = WCS_ATT_Cart::get_cart_subscription_schemes() ) {
+		if ( $subscription_schemes = WCS_ATT_Cart::get_cart_subscription_schemes( 'display' ) ) {
 
 			$active_scheme_key = WCS_ATT_Cart::get_cart_subscription_scheme();
 			$options           = array();

languages/woocommerce-subscribe-all-the-things.pot
@@ -5,7 +5,7 @@ msgstr ""
 "Project-Id-Version: WooCommerce Subscribe All the Things 2.0.0-alpha\n"
 "Report-Msgid-Bugs-To: "
 "https://github.com/Prospress/woocommerce-subscribe-to-all-the-things/issues\n"
-"POT-Creation-Date: 2017-07-13 23:17:27+00:00\n"
+"POT-Creation-Date: 2017-07-18 08:18:32+00:00\n"
 "MIME-Version: 1.0\n"
 "Content-Type: text/plain; charset=utf-8\n"
 "Content-Transfer-Encoding: 8bit\n"
@@ -157,21 +157,21 @@ msgstr ""
 msgid "Remove"
 msgstr ""
 
-#: includes/class-wcs-att-cart.php:531
+#: includes/class-wcs-att-cart.php:537
 msgid "Please choose a new subscription plan."
 msgstr ""
 
-#: includes/class-wcs-att-cart.php:537
+#: includes/class-wcs-att-cart.php:543
 msgid ""
 "The &quot;%1$s&quot; subscription that you signed up for is no longer "
 "available. %2$s"
 msgstr ""
 
-#: includes/class-wcs-att-cart.php:549
+#: includes/class-wcs-att-cart.php:555
 msgid "Please choose a plan to proceed."
 msgstr ""
 
-#: includes/class-wcs-att-cart.php:555
+#: includes/class-wcs-att-cart.php:561
 msgid "&quot;%1$s&quot; is only available with a subscription plan. %2$s"
 msgstr ""
 