905e2b5171f0602a7f6f56496d94ebe66db4d1f1
Manos Psychogyiopoulos <psyx@somewherewarm.gr>
2018-02-12 14:02:38 +0200

    Rewrite abstract module class in OOP fashion

--- includes/modules/abstract/class-wcs-att-abstract-module.php
+++ includes/modules/abstract/class-wcs-att-abstract-module.php
@@ -14,28 +14,83 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /**
  * Abstract class used as the foundation for SATT modules.
+ * Modules are groupings of functionality that SATT uses to attach hooks associated with specific application components.
+ * This is just a way to organize SATT code better.
+ * See 'WCS_ATT::includes', 'WCS_ATT::register_modules' and 'WCS_ATT::register_component_hooks'.
  *
- * @version 2.1.0
+ * @version  2.1.0
  */
 abstract class WCS_ATT_Abstract_Module {
+
+	/**
+	 * Sub-modules to instantiate.
+	 * @var array
+	 */
+	protected $modules = array();
 
 	/**
 	 * Handles module initialization.
 	 *
 	 * @return void
 	 */
-	public static function initialize() {
-		// Use static:: when declaring 'register_hooks' as abstract. Requires PHP 5.3+.
-		self::register_hooks( 'core' );
+	public function __construct() {
+		$this->register_hooks( 'core', false );
+		$this->register_modules();
+		$this->initialize_modules();
 	}
 
 	/**
-	 * Adds module hooks by type.
+	 * Include submodules.
 	 *
-	 * Empty for PHP 5.2 compatibility, otherwise can be delared as abstract.
-	 *
-	 * @param  string  $type
 	 * @return void
 	 */
-	public static function register_hooks( $type ) {}
+	protected function register_modules() {}
+
+	/**
+	 * Initialize submodules.
+	 *
+	 * @return void
+	 */
+	protected function initialize_modules() {
+
+		$modules = array();
+
+		foreach ( $this->modules as $module ) {
+			$modules[] = new $module();
+		}
+
+		$this->modules = $modules;
+	}
+
+	/**
+	 * Adds sub-module hooks by component type.
+	 *
+	 * @param  string  $component
+	 * @return void
+	 */
+	protected function register_module_hooks( $component ) {
+		foreach ( $this->modules as $module ) {
+			$module->register_hooks( $component );
+		}
+	}
+
+	/**
+	 * Adds module hooks by component type.
+	 *
+	 * @param  string   $component
+	 * @param  boolean  $register_module_hooks
+	 * @return void
+	 */
+	public function register_hooks( $component, $register_module_hooks = true ) {
+
+		$fn_name = 'register_' . $component . '_hooks';
+
+		if ( is_callable( array( $this, $fn_name ) ) ) {
+			$this->$fn_name();
+		}
+
+		if ( $register_module_hooks ) {
+			$this->register_module_hooks( $component );
+		}
+	}
 }
