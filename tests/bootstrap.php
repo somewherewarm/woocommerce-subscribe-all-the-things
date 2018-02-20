82ecc0661a449be84aa4ff07e3acae0045672ca1
Jason Conroy <jason@findingsimple.com>
2018-02-20 22:37:54 +1100

    install wc and wcs

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -44,6 +44,9 @@ class WCS_ATT_Unit_Tests_Bootstrap {
 	 */
 	private $wp_plugins_dir;
 
+	// directory storing dependency plugins
+	public $modules_dir;
+
 	/**
 	 * Get the single class instance.
 	 *
@@ -67,6 +70,7 @@ class WCS_ATT_Unit_Tests_Bootstrap {
 	public function __construct() {
 
 		$this->tests_dir      = dirname( __FILE__ );
+		$this->modules_dir    = dirname( dirname( $this->tests_dir ) );
 		$this->wp_tests_dir   = getenv( 'WP_TESTS_DIR' ) ? getenv( 'WP_TESTS_DIR' ) : '/tmp/wordpress-tests-lib';
 		$this->wp_plugins_dir = dirname( dirname( dirname( __FILE__ ) ) );
 
@@ -74,10 +78,105 @@ class WCS_ATT_Unit_Tests_Bootstrap {
 
 		tests_add_filter( 'plugins_loaded', array( $this, 'load_plugins' ) );
 
-		// Load the WCS testing environment.
-		require_once( $this->wp_plugins_dir . '/woocommerce-subscriptions/tests/bootstrap.php' );
+		// load WC
+		tests_add_filter( 'muplugins_loaded', array( $this, 'load_wc' ) );
+
+		// load Subscriptions after $this->load_wc() finishes and calls 'woocommerce_init'
+		tests_add_filter( 'woocommerce_init', array( $this, 'load_wcs' ) );
+
+		// install WC
+		tests_add_filter( 'setup_theme', array( $this, 'install_wc' ) );
+
+		// install WCS
+		tests_add_filter( 'setup_theme', array( $this, 'install_wcs' ) );
+
+		// load the WP testing environment
+		require_once( $this->wp_tests_dir . '/includes/bootstrap.php' );
 
 		$this->includes();
+	}
+
+/**
+	 * Load WooCommerce
+	 *
+	 * @since 2.0
+	 */
+	public function load_wc() {
+		require_once( $this->modules_dir . '/woocommerce/woocommerce.php' );
+	}
+
+	/**
+	 * Load Subscriptions
+	 *
+	 * @since  2.0
+	 */
+	public function load_wcs() {
+		require_once( $this->modules_dir . '/woocommerce-subscriptions/woocommerce-subscriptions.php' );
+	}
+
+	/**
+	 * Load WooCommerce for testing
+	 *
+	 * @since 2.0
+	 */
+	function install_wc() {
+
+		echo "Installing WooCommerce..." . PHP_EOL;
+
+		define( 'WP_UNINSTALL_PLUGIN', true );
+
+		include( $this->modules_dir . '/woocommerce/uninstall.php' );
+
+		WC_Install::install();
+
+		// reload capabilities after install, see https://core.trac.wordpress.org/ticket/28374
+		if ( version_compare( $GLOBALS['wp_version'], '4.9', '>=' ) && method_exists( $GLOBALS['wp_roles'], 'for_site' ) ) {
+			/** @see: https://core.trac.wordpress.org/ticket/38645 */
+			$GLOBALS['wp_roles']->for_site();
+		} elseif ( version_compare( $GLOBALS['wp_version'], '4.7', '>=' ) ) {
+			// Do the right thing based on https://core.trac.wordpress.org/ticket/23016
+			$GLOBALS['wp_roles'] = new WP_Roles();
+		} else {
+			// Fall back to the old method.
+			$GLOBALS['wp_roles']->reinit();
+		}
+
+		// Set Subscriptions install data so that Gifting won't exit early
+		$active_plugins   = get_option( 'active_plugins', array() );
+		$active_plugins[] = 'woocommerce/woocommerce.php';
+		update_option( 'active_plugins', $active_plugins );
+
+		WC()->init();
+
+		echo "WooCommerce Finished Installing..." . PHP_EOL;
+	}
+
+	/**
+	 * Set default values on subscriptions
+	 *
+	 * @since  2.0
+	 */
+	public function install_wcs() {
+
+		echo "Installing Subscriptions..." . PHP_EOL;
+
+		WC_Subscriptions::maybe_activate_woocommerce_subscriptions();
+
+		WC_Subscriptions::load_dependant_classes();
+
+		// Set Subscriptions install data so that Gifting won't exit early
+		$active_plugins   = get_option( 'active_plugins', array() );
+		$active_plugins[] = 'woocommerce-subscriptions/woocommerce-subscriptions.php';
+		update_option( 'active_plugins', $active_plugins );
+		update_option( WC_Subscriptions_Admin::$option_prefix . '_active_version', WC_Subscriptions::$version );
+
+		WC_Subscriptions::register_order_types();
+
+		// set active and inactive subscriber roles
+		update_option( WC_Subscriptions_Admin::$option_prefix . '_subscriber_role', 'subscriber' );
+		update_option( WC_Subscriptions_Admin::$option_prefix . '_cancelled_role', 'customer' );
+
+		echo "Subscriptions Finished Installing..." . PHP_EOL;
 	}
 
 	/**
